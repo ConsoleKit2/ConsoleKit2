25663a88377d76f9e722635bec96627ed0db4e43
William Jon McCann <jmccann@redhat.com>
2008-06-11 11:46:59 -0400

    use object paths instead of strings for signals
    Pointed out by Lennart Poettering

--- src/ck-seat.c
+++ src/ck-seat.c
@@ -928,7 +928,7 @@ ck_seat_class_init (CkSeatClass *klass)
                                                 NULL,
                                                 g_cclosure_marshal_VOID__STRING,
                                                 G_TYPE_NONE,
-                                                1, G_TYPE_STRING);
+                                                1, DBUS_TYPE_G_OBJECT_PATH);
         signals [SESSION_REMOVED] = g_signal_new ("session-removed",
                                                   G_TYPE_FROM_CLASS (object_class),
                                                   G_SIGNAL_RUN_LAST,
@@ -937,7 +937,7 @@ ck_seat_class_init (CkSeatClass *klass)
                                                   NULL,
                                                   g_cclosure_marshal_VOID__STRING,
                                                   G_TYPE_NONE,
-                                                  1, G_TYPE_STRING);
+                                                  1, DBUS_TYPE_G_OBJECT_PATH);
 
         signals [DEVICE_ADDED] = g_signal_new ("device-added",
                                                G_TYPE_FROM_CLASS (object_class),
