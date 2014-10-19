93f6a90d66c68295d3582524446b2c3cb13ccb27
Eric Koegel <eric.koegel@gmail.com>
2014-10-19 12:47:40 +0300

    g_return_val_if_fail can't be used in void function

--- src/ck-inhibit.c
+++ src/ck-inhibit.c
@@ -278,7 +278,7 @@ emit_final_uninhibit_signals (CkInhibit *inhibit)
 {
         CkInhibitPrivate *priv;
 
-        g_return_val_if_fail (CK_IS_INHIBIT (inhibit), -1);
+        g_return_if_fail (CK_IS_INHIBIT (inhibit));
 
         priv = CK_INHIBIT_GET_PRIVATE (inhibit);
 
