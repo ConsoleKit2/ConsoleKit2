0369263e937c2c341136d78b4db9839a85b20d44
OBATA Akio <obache@users.noreply.github.com>
2015-10-10 18:40:43 +0900

    Fix for NetBSD VT device naming scheme

--- tools/ck-get-x11-display-device.c
+++ tools/ck-get-x11-display-device.c
@@ -138,7 +138,11 @@ get_tty_for_display (Display *xdisplay)
                 goto out;
         }
 
+#if defined(__NetBSD__)
+        display = g_strdup_printf ("/dev/ttyE%ld", vt - 1);
+#else
         display = g_strdup_printf ("/dev/tty%ld", vt);
+#endif
 
 out:
         if (return_value != NULL) {
