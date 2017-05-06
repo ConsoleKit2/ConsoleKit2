29c8064e851e2dc689b64d76df24db6b0aec8815
Eric Koegel <eric.koegel@gmail.com>
2017-05-06 06:08:46 +0300

    trivial: update valgrind command
    This way it can easily be hooked into the serivce startup and
    report any leaks for an entire lifecycle.

--- src/valgrind.sh
+++ src/valgrind.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
 export G_DEBUG=gc-friendly
-export G_SLICE=always-malloc
+export G_SLICE=always-malloc,debug-blocks
 
 #valgrind --tool=massif --depth=5  --alloc-fn=g_malloc \
 #  --alloc-fn=g_realloc --alloc-fn=g_try_malloc \
 #  --alloc-fn=g_malloc0 --alloc-fn=g_mem_chunk_alloc \
-#  console-kit-daemon --no-daemon --timed-exit
+#   --log-file=/var/log/ck2-valgrind-massif.log \
+#  console-kit-daemon
 
 valgrind --tool=memcheck --leak-check=full --show-reachable=yes \
-  console-kit-daemon --no-daemon --timed-exit
+    --leak-resolution=high --num-callers=500 \
+    --track-origins=yes --read-var-info=yes \
+    --show-leak-kinds=all --log-file=/var/log/ck2-valgrind-memcheck.log \
+  console-kit-daemon
