55942f1a8f5a8c15b903a782e6192721c98b1a9a
Robert Nagy <robert@openbsd.org>
2025-10-03 09:08:27 +0200

    cast 3rd arg of console_kit_seat_complete_get_sessions to (const gchar **)
    fixes #158

--- src/ck-seat.c
+++ src/ck-seat.c
@@ -1054,7 +1054,7 @@ dbus_get_sessions (ConsoleKitSeat        *ckseat,
         }
 
         g_ptr_array_add (sessions, NULL);
-        console_kit_seat_complete_get_sessions (ckseat, context, sessions->pdata);
+        console_kit_seat_complete_get_sessions (ckseat, context, (const gchar **)sessions->pdata);
         g_ptr_array_unref (sessions);
         return TRUE;
 }
